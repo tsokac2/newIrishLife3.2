4ffa55baea217688b7b3269aa0eee31d1556d630
tsokac2 <tsokac2@gmail.com>
2021-02-26 15:37:48 +0000

    add

--- forms.py
+++ forms.py
@@ -57,9 +57,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField("LOGIN")
 
 class AddTip(FlaskForm):
-    tip_category = SelectField('tip_category', choices=["Trip", "Work", "Study", "Food"], 
-                                validators=[DataRequired("Select a category")])
-
     tip_title = StringField("tip_title", validators=[DataRequired("Enter a tip title"), 
                                 Length(min=5, message="Must be more that 5 character!"),
                                 Length(max=50, message="Max length 50 characters!")],
